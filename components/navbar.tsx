0e39e4104afbf51134a6f0cf3815a8ebc5cae11b
mkxdevx <mikaylaesparza06@gmail.com>
2026-08-09 21:46:55 -0500

    navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,10 +1,30 @@
 import { Briefcase } from "lucide-react";
+import Link from "next/link";
+import { Button } from "./ui/button";
 
 export default function Navbar() {
   return (
-    <nav>
-      <div>
-        <Briefcase />
+    <nav className="border-b border-gray-200 bg-white">
+      <div className="container mx-auto flex h-16 items-center px-4">
+        <Link
+          href="/"
+          className="text-primary flex items-center gap-2 text-xl font-semibold"
+        >
+          <Briefcase />
+          Job Tracker
+        </Link>
+        <div>
+          <Link href="/sign-in">
+            <Button variant="ghost" className="text-gray-700 hover:text-black">
+              Log In
+            </Button>
+          </Link>
+          <Link href="/sign-up">
+            <Button className="bg-primary hover:primary/90">
+              Start for free
+            </Button>
+          </Link>
+        </div>
       </div>
     </nav>
   );
